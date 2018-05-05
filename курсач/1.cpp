59636b281505f32a376e58212c91436a8d69598a
stiveknight <stiveknight@users.noreply.github.com>
2018-05-06 01:35:15 +0400

    +1

--- курсач/1.cpp
+++ курсач/1.cpp
@@ -17,6 +17,7 @@ vector <int> v_rad;
 vector <int> v_diam;
 vector <int> v_duo;
 vector <int> v_m;
+vector <int> v_exp;
 
 vector <int> used;
 vector <int> dolya;
@@ -73,15 +74,17 @@ void komp_sv(vector <vector <int>> g) {
 		}
 	}
 	used.clear();
-	cout << endl << "Связный? ";
+	//cout << endl << "Связный? ";
 	if (komp == 1) {
-		cout << "Да"; v_sv.push_back(1);
+		//cout << "Да"; 
+		v_sv.push_back(1);
 	}
 	else {
-		cout << "Нет"; v_sv.push_back(-1);
+		//cout << "Нет"; 
+		v_sv.push_back(-1);
 	}
-	cout << endl;
-	cout << "Количество компонент связности: " << komp;
+	//cout << endl;
+	//cout << "Количество компонент связности: " << komp;
 	v_komp.push_back(komp);
 	return;
 }
@@ -94,16 +97,17 @@ void obhv(vector <vector <int>> g) {
 		}
 	used.clear();
 
-	cout << endl;
+	//cout << endl;
 	if (obh.size() == 0) {
-		cout << "Обхват равен бесконечности"; v_obh.push_back(-1);
+		//cout << "Обхват равен бесконечности"; 
+		v_obh.push_back(-1);
 	}
 	else {
 		int min_ob = obh[0];
 		for (int i = 0; i < obh.size(); i++) {
 			if (obh[i] < min_ob) min_ob = obh[i];
 		}
-		cout << "Обхват: " << min_ob;
+		//cout << "Обхват: " << min_ob;
 		v_obh.push_back(min_ob);
 	}
 	p.clear();
@@ -118,7 +122,7 @@ void treo(vector <vector <int>> g) {
 		for (int b = a + 1; b < n; b++)
 			for (int c = b + 1; c < n; c++)
 				if (g[a][b] != 0 && g[b][c] != 0 && g[a][c] != 0) cnt++;
-	cout << endl << "Количество треугольников: " << cnt;
+	//cout << endl << "Количество треугольников: " << cnt;
 	v_treo.push_back(cnt);
 	return;
 }
@@ -144,9 +148,9 @@ void rad_diam(vector <vector <int>> g) {
 		rad = min(rad, e[i]);
 		diam = max(diam, e[i]);
 	}
-	cout << endl << "Радиус = " << rad;
+	//cout << endl << "Радиус = " << rad;
 	v_rad.push_back(rad);
-	cout << endl << "Диаметр = " << diam;
+	//cout << endl << "Диаметр = " << diam;
 	v_diam.push_back(diam);
 	return;
 }
@@ -184,7 +188,7 @@ void duo(vector <vector <int>> g) {
 	}
 	for (int i = 0; i < n; i++) {
 		if (!ok) {
-			cout << endl << "Граф не двудольный";
+			//cout << endl << "Граф не двудольный";
 			v_duo.push_back(-1);
 			dolya.clear(); used.clear();
 			return;
@@ -192,21 +196,83 @@ void duo(vector <vector <int>> g) {
 		else if (used[i] == 0) bfs(i);
 	}
 	if (ok) {
-		cout << endl << "Граф двудольный"; v_duo.push_back(1);
+		//cout << endl << "Граф двудольный"; 
+		v_duo.push_back(1);
 	}
 	else {
-		cout << endl << "Граф не двудольный";  v_duo.push_back(-1);
+		//cout << endl << "Граф не двудольный";  
+		v_duo.push_back(-1);
 	}
 	dolya.clear();
 	used.clear();
 	return;
 }
 
+void ex(vector <vector <int>> &g) {
+	int e = 1;
+	int emax = pow(n, 2) - 2 * n + 2;
+	vector <vector <int>> gg(g);
+	vector<vector<int>> c(n, vector<int>(n, 0));
+	
+	bool b = true;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (g[i][j] == 0 && i!=j) {
+				b = false;
+				break;
+			}
+		}
+	}
+
+	if (b) {
+		v_exp.push_back(e);
+		cout << "Граф примитивный с экспонентой = " << e << endl;
+		return;
+	}
+	else {
+		bool bb = true;
+		for (int i = 2; i <= emax; i++) {
+			for (int j = 1; j < i; j++) {
+				for (int ii = 0; ii < n; ii++) {
+					for (int jj = 0; jj < n; jj++) {
+						c[ii][jj] = 0;
+						for (int k = 0; k < n; k++) {
+							c[ii][jj] += g[ii][k] * gg[k][jj];
+						}
+					}
+				}
+			}
+			gg = c;
+			bb = true;
+			for (int ii = 0; ii < n && bb; ii++) {
+				for (int jj = 0; jj < n; jj++) {
+					if (c[ii][jj] == 0 && ii!=jj) {
+						bb = false;
+						break;
+					}
+				}
+			}
+			if (bb) {
+				v_exp.push_back(i);
+				cout << "Граф примитивный с экспонентой = " << i << endl;
+				return;
+			}
+		}
+		if (!bb) {
+			v_exp.push_back(0);
+			cout << "Граф не примитивный" << endl;
+			return;
+		}
+	}
+
+}
+
 int main() {
 	int kolvo = 0;
 	setlocale(LC_ALL, "Russian");
 	//считывание матриц смежности графов в цикле
-	ifstream in("graphs5.txt");
+	ifstream in("graphs6.txt");
 	in >> n;
 	getline(in, str);
 	while (getline(in, str)) {
@@ -228,12 +294,12 @@ int main() {
 			}
 		}
 		for (int i = 0; i < n; i++) {
-			cout << endl;
-			for (int j = 0; j < n; j++)
-				cout << g[i][j];
+			//cout << endl;
+			//for (int j = 0; j < n; j++)
+				//cout << g[i][j];
 		}
-		cout << endl << "n = " << n;
-		cout << endl << "m = " << m / 2;
+		//cout << endl << "n = " << n;
+		//cout << endl << "m = " << m / 2;
 		v_m.push_back(m / 2);
 
 		//связный ли и количество компонент
@@ -246,53 +312,57 @@ int main() {
 		rad_diam(g);
 		// двудольность
 		duo(g);
+		
+		ex(g);
+
 		kolvo++;
 
-		cout << endl;
+		//cout << endl;
 		g.clear();
 		d.clear();
 	}
-	set <int> s_sv;
-	set <int> s_komp;
-	set <int> s_obh;
-	set <int> s_treo;
-	set <int> s_rad;
-	set <int> s_diam;
-	set <int> s_duo;
-	set <int> s_m;
-	for (int i = 0; i < v_sv.size(); i++) {
-		s_sv.insert(v_sv[i]);
-		s_komp.insert(v_komp[i]);
-		s_obh.insert(v_obh[i]);
-		s_treo.insert(v_treo[i]);
-		s_rad.insert(v_rad[i]);
-		s_diam.insert(v_diam[i]);
-		s_duo.insert(v_duo[i]);
-		s_m.insert(v_m[i]);
-	}
-	vector <int> klass;
-	unsigned int start_time = clock();
-	set <int>::iterator it1, it2, it3, it4, it5, it6, it7;
-	int t = 0;
-	for (it1 = s_komp.begin(); it1 != s_komp.end(); it1++)
-		for (it2 = s_m.begin(); it2 != s_m.end(); it2++)
-			for (it3 = s_obh.begin(); it3 != s_obh.end(); it3++)
-				for (it4 = s_treo.begin(); it4 != s_treo.end(); it4++)
-					for (it5 = s_duo.begin(); it5 != s_duo.end(); it5++)
-						for (it6 = s_rad.begin(); it6 != s_rad.end(); it6++)
-							for (it7 = s_diam.begin(); it7 != s_diam.end(); it7++)
-							{
-								for (int k = 0; k < kolvo; k++) {
-									if (v_komp[k] == *it1 && v_m[k] == *it2 && v_obh[k] == *it3 && v_treo[k] == *it4 && v_duo[k] == *it5 && v_rad[k] == *it6 && v_diam[k] == *it7)
-										t++;
-								}
-								if (t != 0) klass.push_back(t);
-								t = 0;
-							}
-	unsigned int end_time = clock();
-	unsigned int search_time = end_time - start_time;
-	cout << klass.size();
-	cout << endl << (float(search_time)) / CLOCKS_PER_SEC;
+	//set <int> s_sv;
+	//set <int> s_komp;
+	//set <int> s_obh;
+	//set <int> s_treo;
+	//set <int> s_rad;
+	//set <int> s_diam;
+	//set <int> s_duo;
+	//set <int> s_m;
+	//for (int i = 0; i < v_sv.size(); i++) {
+	//	s_sv.insert(v_sv[i]);
+	//	s_komp.insert(v_komp[i]);
+	//	s_obh.insert(v_obh[i]);
+	//	s_treo.insert(v_treo[i]);
+	//	s_rad.insert(v_rad[i]);
+	//	s_diam.insert(v_diam[i]);
+	//	s_duo.insert(v_duo[i]);
+	//	s_m.insert(v_m[i]);
+	//}
+	//vector <int> klass;
+	//unsigned int start_time = clock();
+	//set <int>::iterator it1, it2, it3, it4, it5, it6, it7;
+	//int t = 0;
+	//for (it1 = s_komp.begin(); it1 != s_komp.end(); it1++)
+	//	for (it2 = s_m.begin(); it2 != s_m.end(); it2++)
+	//		for (it3 = s_obh.begin(); it3 != s_obh.end(); it3++)
+	//			for (it4 = s_treo.begin(); it4 != s_treo.end(); it4++)
+	//				for (it5 = s_duo.begin(); it5 != s_duo.end(); it5++)
+	//					for (it6 = s_rad.begin(); it6 != s_rad.end(); it6++)
+	//						for (it7 = s_diam.begin(); it7 != s_diam.end(); it7++)
+	//						{
+	//							for (int k = 0; k < kolvo; k++) {
+	//								if (v_komp[k] == *it1 && v_m[k] == *it2 && v_obh[k] == *it3 && v_treo[k] == *it4 &&
+	//									v_duo[k] == *it5 && v_rad[k] == *it6 && v_diam[k] == *it7)
+	//									t++;
+	//							}
+	//							if (t != 0) klass.push_back(t);
+	//							t = 0;
+	//						}
+	//unsigned int end_time = clock();
+	//unsigned int search_time = end_time - start_time;
+	//cout << klass.size();
+	//cout << endl << (float(search_time)) / CLOCKS_PER_SEC;
 	system("pause");
 	return 0;
 }
